03aa39bb251fd4ee1eda75f8091e17608cd3d28f
Alfeu <alfeu.buriti@gmail.com>
2021-08-30 17:20:49 -0300

    first commmit

---
+++ src/main/java/com/fluorita/personapi/controller/PersonController.java
@@ -0,0 +1,16 @@
+package com.fluorita.personapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/vi/people")
+public class PersonController {
+
+    @GetMapping
+    public String getBook() {
+        return "API Test!";
+    }
+
+}
